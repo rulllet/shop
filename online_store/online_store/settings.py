46d3161049cdde3a54404ceff98a9d571f9e8cf5
rulllet <112080343+rulllet@users.noreply.github.com>
2022-08-27 22:49:31 +0300

    0.2

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*tsz$3n90(d7w^udv#tns&pqu!xe9fg0jkqr3$a&p8qd1)5+!i"
+SECRET_KEY = "no key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
